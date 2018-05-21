a402ae90b0469ed177c2a8a059d9aa6eb3e03e86
lonnyhuang <lonnyhuang@upchina.com>
2018-05-21 20:41:08 +0800

    换成es5 写法

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-const san = require('san');
+var san = require('san');
 
 module.exports = san.defineComponent({
-    template: `
-    <template on-touchstart="touchStartHandler" on-touchend="touchEndHandler">
-        <slot>
-        </slot>
-    </template>
-    `,
-    initData() {
+    template: 
+    '<template>'+
+        '<slot on-touchstart="touchStartHandler" on-touchend="touchEndHandler">'+
+        '</slot>'+
+    '</template>',
+    initData: function() {
         return {
             x: null,
             y: null,
             scrollTop: null
         };
     },
-    touchStartHandler(evt) {
+    touchStartHandler: function(evt) {
         this.data.set('x', evt.touches[0].pageX);
         this.data.set('y', evt.touches[0].pageY);
         this.data.set('scrollTop', document.body.scrollTop);
     },
-    touchEndHandler(evt) {
-        const x = this.data.get('x');
-        const y = this.data.get('y');
-        const scrollTop = this.data.get('scrollTop');
+    touchEndHandler: function(evt) {
+        var x = this.data.get('x');
+        var y = this.data.get('y');
+        var scrollTop = this.data.get('scrollTop');
         if (Math.abs(evt.changedTouches[0].pageX - x) < 30 && Math.abs(evt.changedTouches[0].pageY - y) < 30 && Math.abs(document.body.scrollTop - scrollTop) < 30) {
             this.fire('tap', evt);
         }
